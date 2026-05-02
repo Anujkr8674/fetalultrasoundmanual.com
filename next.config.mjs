53bd84cf52182beb861ce0647c3701873c85bf73
Anuj Kumar <anujkr8674@example.com>
2026-05-02 14:08:26 +0530

    update code

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
-
-
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   output: "standalone",
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true,
+//   },
 // };
 
 // export default nextConfig;
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+};
+
+export default nextConfig;
